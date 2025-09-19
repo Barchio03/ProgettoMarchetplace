0e40059e599029b1afd6d983a66316f660a1eab9
Luca Barchiesi <lucabarchiesi03@gmail.com>
2025-09-19 14:30:38 +0200

    Create PurchaseHandler.java

---
+++ IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Controllers/PurchaseHandler.java
@@ -0,0 +1,20 @@
+package unicam.IdSProject.Controllers;
+import unicam.IdSProject.Controllers.BuyerController;
+
+/**
+*
+* This class implements the payment methods for buying Events and Products
+*
+* @author Erika Aguiari, Ilaria Morettini, Luca Barchiesi
+*
+*/
+public class PurchaseHandler(){
+
+  public PurchaseHandler() { }
+
+  public boolean pay(){
+    return true;
+  }
+
+  
+}
