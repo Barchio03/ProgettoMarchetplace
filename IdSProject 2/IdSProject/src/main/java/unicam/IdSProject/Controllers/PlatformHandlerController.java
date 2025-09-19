f74e61a7736b8d50ac35256a6019d4516dda8ba1
Luca Barchiesi <lucabarchiesi03@gmail.com>
2025-09-19 14:48:21 +0200

    Update PlatformHandlerController.java

--- IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Controllers/PlatformHandlerController.java
+++ IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Controllers/PlatformHandlerController.java
@@ -1,6 +1,6 @@
 package unicam.IdSProject.Controllers;
-
 import unicam.IdSProject.Models.PlatformHandler;
+import unicam.IdSProject.Models.Mailbox;
 import java.util.ArrayList;
 
 /**
@@ -8,9 +8,11 @@ import java.util.ArrayList;
 * This class implements the methods for a PlatFormHandler used to interact with the marketplace.
 *
 * @author Erika Aguiari, Ilaria Morettini, Luca Barchiesi
+*
 */
 public class PlatformHandlerController {
 
+    private Mailbox mailbox;
     private final PlatformHandler platformHandler;
 
     //Si potrebbe far estendere un'interfaccia User a tutti coloro che utilizzano la piattaforma
@@ -21,19 +23,44 @@ public class PlatformHandlerController {
 
     //private ArrayList<User> allUsers;
 
+    /**
+    * This method creates a new PlatformHandler object
+    */
     public PlatformHandlerController(PlatformHandler platformHandler){
         this.platformHandler = platformHandler;
     }
 
 
+    
+    //      |                                |
+    //      |  Tutti metodi da implementare  |
+    //      V                                V
+
+
+    /**
+    * This method blocks the application to a specific user
+    */
     public void blockUser() {}
 
+    /**
+    * This method unblocks a previously blocked user
+    */
     public void unblockUser() {}
 
+    /**
+    * This method appoints a new Curator
+    *
+    * @return true if it appoints a new Curator, false otherwise
+    */
     public boolean appointCurator() {
         return false;
     }
 
+    /**
+    * This method verifies if a receipt has been received
+    *
+    * @return true if the receipt is received, false otherwise
+    */
     public boolean verifyReceivedReceipt() {
         return false;
     }
