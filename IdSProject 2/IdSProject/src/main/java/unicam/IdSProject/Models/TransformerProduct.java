93d32e4421771c8effabc9caa5f9f57b8fb45026
Luca Barchiesi <lucabarchiesi03@gmail.com>
2025-09-19 13:03:35 +0200

    Add comments to TransformerProduct.java

--- IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Models/TransformerProduct.java
+++ IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Models/TransformerProduct.java
@@ -1,12 +1,23 @@
 package unicam.IdSProject.Models;
 
+/**
+*
+* This class represents a Product made by a Transformer
+*
+* @author Erika Aguiari, Ilaria Morettini, Luca Barchiesi
+*
+*/
 public class TransformerProduct extends Product {
 
     private Product sourceProduct;
 
+    /**
+    * This method creates a new TrasformerProduct object
+    */
     public TransformerProduct() {
         super();
     }
+
 
     public Product getSourceProduct() {
         return sourceProduct;
