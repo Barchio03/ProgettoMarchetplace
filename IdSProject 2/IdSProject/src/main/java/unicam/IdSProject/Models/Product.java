5ae47feaabfd50af63b9259732b1b1aa1ee0af7f
Luca Barchiesi <lucabarchiesi03@gmail.com>
2025-09-19 12:36:13 +0200

    Add comments to Product.java

--- IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Models/Product.java
+++ IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Models/Product.java
@@ -5,129 +5,77 @@ import unicam.IdSProject.Visitor;
 
 /**
 *
-* This class represents a product.
+* This class represents a Product.
 *
 * @author Erika Aguiari, Ilaria Morettini, Luca Barchiesi
 *
 */
 public abstract class Product implements Visitable {
 
-
     private String name;
-
-
     private float price;
-
-
     private String description;
-
-
     private String distributorDescription;
-
-
     private int stockNumber;
-
-
     private Seller creator;
-
     private int id;
-
     private static int currentId;
 
 
     /**
-    *
-    * This method creates an object Product.
-    *
-    * @return an object Product.
-    *
+    * This method creates a new Product object.
     */
     public Product() {}
 
+    
     private static int getCurrentId() {
         return currentId;
     }
 
-    /**
-     * @return the name of the product
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name, the name of the product
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return the price of the product
-     */
     public float getPrice() {
         return price;
     }
 
-    /**
-     * @param price, the price of the product
-     */
     public void setPrice(float price) {
         this.price = price;
     }
 
-    /**
-     * @return the description of the product
-     */
     public String getDescription() {
         return description;
     }
 
-    /**
-     * @param description, the description of the product
-     */
     public void setDescription(String description) {
         this.description = description;
     }
 
-    /**
-     * @return the descrioption added by the Distributor
-     */
     public String getDistributorDescription() {
         return distributorDescription;
     }
 
-    /**
-     * @param distributorDescription, the description added by the Distributor
-     */
     public void setDistributorDescription(String distributorDescription) {
         this.distributorDescription = distributorDescription;
     }
 
-    /**
-     * @return the number of stock of the product
-     */
     public int getStockNumber() {
         return stockNumber;
     }
 
-    /**
-     * @param stockNumber, the number of stock of the product
-     */
     public void setStockNumber(int stockNumber) {
         this.stockNumber = stockNumber;
     }
 
-    /**
-     * @return the creator of the product
-     */
     public Seller getCreator() {
         return creator;
     }
 
-    /**
-     * @return the id of the product
-     */
     public int getId() {
         return id;
     }
@@ -136,6 +84,7 @@ public abstract class Product implements Visitable {
         this.creator = creator;
     }
 
+    
 
     @Override
     public void accept(Visitor visitor) {
