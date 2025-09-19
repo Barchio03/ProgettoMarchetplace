3d75dd27a8f51cbaa4c684767b3cada553f6d50b
Luca Barchiesi <lucabarchiesi03@gmail.com>
2025-09-19 12:48:12 +0200

    Add comments to QuantifiedProduct.java

--- IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Models/QuantifiedProduct.java
+++ IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Models/QuantifiedProduct.java
@@ -1,33 +1,20 @@
 package unicam.IdSProject.Models;
 
 /**
- * This class represents the product inside a shopping cart
- *
- * @author Ilaria Morettini, Luca Barchiesi, Erika Aguiari
- */
+*
+* This class represents the Product inside a Shopping Cart
+*
+* @author Erika Aguiari, Ilaria Morettini, Luca Barchiesi
+*
+*/
 public class QuantifiedProduct {
 
-    /**
-     * The product to buy
-     */
     private Product product;
-
-    /**
-     * The number of stocks to buy
-     */
     private int stockNumber;
-
-    /**
-     * The total price, obtained by multiplying the stockNumber
-     * with the price of the singular product
-     */
     private final double totalPrice;
 
     /**
-     * Creates a new quantified product to add to a shopping cart
-     *
-     * @param product, the product
-     * @param stockNumber, the number of stocks chosen
+     * This method creates a new QuantifiedProduct object
      */
     public QuantifiedProduct(Product product, int stockNumber) {
         this.product=product;
@@ -35,37 +22,23 @@ public class QuantifiedProduct {
         this.totalPrice=product.getPrice() * stockNumber;
     }
 
-    /**
-     * @return the product
-     */
+    
     public Product getProduct() {
         return product;
     }
 
-    /**
-     * @param product, the product
-     */
     public void setProduct(Product product) {
         this.product = product;
     }
 
-    /**
-     * @return the stock number
-     */
     public int getStockNumber() {
         return stockNumber;
     }
 
-    /**
-     * @param stockNumber, the number of stocks chosen
-     */
     public void setStockNumber(int stockNumber) {
         this.stockNumber = stockNumber;
     }
 
-    /**
-     * @return the total price
-     */
     public double getTotalPrice() { return totalPrice; }
 
 }
