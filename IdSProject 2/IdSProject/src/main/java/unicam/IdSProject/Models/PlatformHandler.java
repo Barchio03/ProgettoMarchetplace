aa41593512a78078d93c9a402f791e0561da13e0
Luca Barchiesi <lucabarchiesi03@gmail.com>
2025-09-19 12:31:45 +0200

    Add comments to PlatformHandler.java

--- IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Models/PlatformHandler.java
+++ IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Models/PlatformHandler.java
@@ -3,15 +3,26 @@ package unicam.IdSProject.Models;
 import unicam.IdSProject.Models.Mailbox;
 import unicam.IdSProject.Controllers.Curator;
 
+/**
+*
+* This class represents the Platform Handler of the marketplace
+*
+* @author Aguiari Erika, Ilaria Morettini, Barchiesi Luca
+*
+*/
 public class PlatformHandler {
 
     private Mailbox mailbox;
-
     private List<Curator> curators;
 
+    /**
+    * This method creates a new PlatformHandler object
+    */
     public PlatformHandler(Mailbox mailbox){
         this.mailbox = mailbox
     }
+
+
     
     public Mailbox getMailbox() {
         return mailbox;
