403daa5e816c7315a5719817b323bd2e120d3814
Luca Barchiesi <lucabarchiesi03@gmail.com>
2025-09-19 13:12:19 +0200

    Add comments to Visitor.java

--- IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Visitor.java
+++ IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Visitor.java
@@ -1,22 +1,25 @@
 package unicam.IdSProject;
-
 import unicam.IdSProject.Models.Event;
 import unicam.IdSProject.Models.Product;
 
 /**
- * This interface declares the methods to visit the concrete visitables (those who implement the Visitable interface)
- */
+*
+* This interface declares the methods to visit the concrete visitables (those who implement the Visitable interface)
+*
+* @author Erika Aguiari, Ilaria Morettini, Luca Barchiesi
+*
+*/
 public interface Visitor {
 
     /**
-     * Visits a product
+     * This method visits a Product
      *
      * @param product
      */
     void visit(Product product);
 
     /**
-     * Visits an event
+     * This method visits an Event
      *
      * @param event
      */
