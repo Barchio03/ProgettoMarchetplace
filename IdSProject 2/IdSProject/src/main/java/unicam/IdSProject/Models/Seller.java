a0792eb49b1421a66f3c37cb03813b349421e561
Luca Barchiesi <lucabarchiesi03@gmail.com>
2025-09-19 12:55:46 +0200

    Add comments to Seller.java

--- IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Models/Seller.java
+++ IdSProject 2/IdSProject/src/main/java/unicam/IdSProject/Models/Seller.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 /**
 *
-* This class is used to represent a seller.
+* This class represents a Seller
 *
 * @author Erika Aguiari, Ilaria Morettini, Luca Barchiesi
 *
@@ -15,101 +15,66 @@ import java.util.ArrayList;
 public class Seller {
 
     private String name;
-
     private String description;
-
     private Location location;
-
     private ArrayList<Tag> tags;
-
     private final Mailbox mailbox;
-
     private int id;
-
     private static int currentId;
-
-
 
     
     /**
-    * This method creates an object Seller.
+    * This method creates a new Seller object
     */
     public Seller(String name, String description ) {
         this.mailbox=new Mailbox();
     }
 
+    
     private static int getCurrentId() {
         return currentId;
     }
 
-    /**
-     * @return the name of the seller
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name, the name of the seller
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return the description of the seller
-     */
     public String getDescription() {
         return description;
     }
 
-    /**
-     * @param description, the description of the seller
-     */
     public void setDescription(String description) {
         this.description = description;
     }
 
-    /**
-     * @return the location of the seller
-     */
     public Location getLocation() {
         return location;
     }
 
-    /**
-     * @param location, the location of the seller
-     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
-    /**
-     * @return the tag assigned to the seller
-     */
     public ArrayList<Tag> getTags() {
         return tags;
     }
 
-    /**
-     * @return the id of the seller
-     */
     public int getId() {
         return id;
     }
 
-    /**
-     * @return the mailbox of the seller
-     */
     public Mailbox getMailbox() {
         return mailbox;
     }
 
-
+    @Override
     public String toString(){
-        return "Nome: "+this.getName()+
-                "\nDescrizione: "+this.getDescription()+
+        return "Nome: "+this.getName() +
+                "\nDescrizione: "+this.getDescription() +
                 "\nLocation: "+this.getLocation().toString();
-
     }
 }
